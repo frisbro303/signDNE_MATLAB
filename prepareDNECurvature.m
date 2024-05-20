098e425bab51e2caced0961c1a114aae9e2b26e8
wush303 <73933966+wush303@users.noreply.github.com>
2024-05-20 17:24:18 +0200

    Add files via upload

--- prepareDNECurvature.m
+++ prepareDNECurvature.m
@@ -2,11 +2,6 @@
 clear; clc;
 %%
 pathSetup();
-% pathSetup(BaseDirectory) %or provide a specified base directory
-
-
-%noise_level = 0.05;%0.001;
-% load mesh
 
 
 load('W_colormap.mat');
@@ -15,10 +10,6 @@ load('H_colormap.mat');
 load('CustomColormapbluered.mat');
 
 
-%%
-% new dne
-% figure;
-%colormap(jet(4096));
 
 IDList = [1 2 3 4 5 6];
 nIDs = length(IDList);
@@ -26,8 +17,6 @@ mesh_names = {'normal', 'low', 'high', 'smooth', '10^{-3} noise', '2 \cdot 10^{-
 
 positiveDNEs = zeros(nIDs, 1);
 negativeDNEs = zeros(nIDs, 1);
-%DNEratio = zeros(nIDs,1);
-%smoothDNEratio = zeros(nIDs,1);
 
 t = tiledlayout(1, 6, "TileSpacing", "tight");
 noise_level = 0.001 * [1:3];
@@ -45,20 +34,14 @@ for idCnt = 1:nIDs
     Options.distInfo = 'Geodeisic';
     Options.cutThresh = 0;
     bandwidth = 0.06;
-    %Options.smoothing = 1;
-    %meshname = [mesh_names(idCnt) '.ply'];
 
     G.Centralize('ScaleArea');
 
     H = ariaDNE(G, bandwidth, Options);
-    disp(H.dne);
     positiveDNEs(idCnt) = H.positiveDNE;
     negativeDNEs(idCnt) = H.negativeDNE;
     
-    
-   
-    %subplot(1, 6, idCnt)
-    %tight_subplot(1, 6, [0.3, 0.3]);
+    % Patch the tooth
     nexttile
     
     hP = patch('vertices',G.V','faces',G.F');
@@ -80,13 +63,7 @@ for idCnt = 1:nIDs
 end
 
 linkPropH = linkprop(hDNE(:), {'CameraUpVector', 'CameraPosition', 'CameraTarget', 'CameraViewAngle', 'ColorMap'});
-% hDNE(2,1).CLim = [0 2000];
 
-
-%linkPropCLim = linkprop(hDNE(:), 'CLim');
-%hDNE(2,1).CLim = [0 2000];
-%colormap(H_colormap);
-%
 colormap(CustomColormapbluered)
 
 positiveDNE_norm = positiveDNEs/positiveDNEs(1);
@@ -101,6 +78,5 @@ figure;
 bar(mesh_names, negativeDNE_norm)
 
 legend('negative');
-%colormap(winter);
-%colormap(H_colormap);
+
 
