3fa2e677f9d6e7f659f88ed3ba87e2550adddc09
wush303 <felix@risbro.dk>
2024-09-07 17:10:18 +0200

    Updated example

--- example.m
+++ example.m
@@ -4,7 +4,6 @@ pathSetup();
 % pathSetup(BaseDirectory) %or provide a specified base directory
 
 % compute ariaDNE
-%values = zeros(6:3);
 Options.distInfo = 'Euclidean';
 Options.cutThresh = 0;
 bandwidth = 0.08;
