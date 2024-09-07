495b8414711e3f91c3be2cd014a9cfa8cc223e6e
wush303 <felix@risbro.dk>
2024-09-07 14:21:03 +0200

    Added data to computeDNEfolder

--- computeDNEfolder.m
+++ computeDNEfolder.m
@@ -3,10 +3,10 @@ function result = computeDNEfolder(foldername, bandwidth, Options)
 
     filteredFileNameList = fileNameList(~endsWith(fileNameList, '_watertight'));
 
-    result = zeros(length(filteredFileNameList), 3);
+    result = zeros(length(filteredFileNameList), 6);
     for i = 1:length(filteredFileNameList)
         meshname = [foldername '/' filteredFileNameList{i} suffix];
         H = ariaDNE(meshname, bandwidth, Options);
-        result(i, :) = [H.DNE, H.positiveDNE, H.negativeDNE];
+        result(i, :) = [H.DNE, H.positiveDNE, H.negativeDNE, H.surface_area, H.positive_surface_area, H.negative_surface_area];
     end
 end
